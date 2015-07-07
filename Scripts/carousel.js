3cf76ae7ce35084710f9bbe5ed2331bbcdae5492
dchen71 <dchen12812@gmail.com>
2015-07-07 15:09:11 -0700

    changed ul buttons to be able to change image and active class

--- Scripts/carousel.js
+++ Scripts/carousel.js
@@ -29,3 +29,52 @@ $('.right').click(function(){
 		pos += 1;
 	$('.item').append(img[pos]);
 });
+
+//Function to change image
+function changeImg(){
+	$('.item').empty();
+	$('.item').append(img[pos]);
+};
+
+//Function to change active class
+function changeActive(act){
+	$('.active').removeClass();
+	$(act).addClass('active');
+}
+
+//Click handlers to change image and active class
+$('#one').click(function(){
+	pos = 0;
+	changeImg();
+	changeActive('#one');
+});
+
+$('#two').click(function(){
+	pos = 1;
+	changeImg();;
+	changeActive('#two');
+});
+
+$('#three').click(function(){
+	pos = 2;
+	changeImg();;
+	changeActive('#three');
+});
+
+$('#four').click(function(){
+	pos = 3;
+	changeImg();;
+	changeActive('#four');
+});
+
+$('#five').click(function(){
+	pos = 4;
+	changeImg();;
+	changeActive('#five');
+});
+
+$('#six').click(function(){
+	pos = 5;
+	changeImg();;
+	changeActive('#six');
+});
